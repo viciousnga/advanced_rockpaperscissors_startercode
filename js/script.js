3a98b0d0a62f4b34930048f89e6c3671dd90143b
Ngawang Salaka <ngawangs1180@queensvoc.org>
2018-01-22 22:02:48 +0000

    rpsCool

--- js/script.js
+++ js/script.js
@@ -7,12 +7,11 @@
         // Create your document ready function. 
         // Create a click hander for when the "Shoot!" button is clicked, takes in the user choice from the input field, 
         // Display the user choice to the output screen
-        <script type="text/javascript">
-        var userchoice= "";
-        var computerchoice = ""; 
-        var winner = "";]
-        var choices ="rock" "paper" "scissors";
-</script>
+        var userchoice= "rock";
+        var computerchoice = "paper"; 
+        var winner = "paper";
+        var choices =["rock", "paper", "scissors"];
+console.log(Math.random()*choices.length);
 
 //Class 3:  
         // Randomly choose among 'rock', 'paper', or 'scissors' from the list of choices 
@@ -20,6 +19,64 @@
         // Compare the user choice and the computer choice to determine who won. 
         // Display the user winner to the output screen 
         // Clear the input box for the next choice
+$(document).ready(function() {
+  /*
+  $("#input").on('click', function() {
+        $('#input').val();
+  })
+*/
+
+        $("#shoot").click(function() {
+            var userinput= $("#userInput").val();
+            var computerinput= getRandomChoice();
+        $("#computerbox").val(computerinput);
+            var win= selectWinner(userinput,computerinput);
+            alert(win);
+        });
+        function getRandomChoice() {
+            var choices = ["rock","paper","scissors"];
+            var randomIndex = Math.floor(Math.random() * choices.length);
+            var randomChoice = choices[randomIndex];
+            return randomChoice; 
+        }
+        
+
+
+function selectWinner(userChoice, computerChoice) {
+        if (userChoice === "scissors") {
+                if (computerChoice === "rock") {
+                    return "computer wins";
+                } else if (computerChoice === "paper") {
+                    return "user wins";
+                } else {
+                    return "it's a tie";
+                }
+        }
+        if (userChoice === "rock") {
+                if (computerChoice === "scissors") {
+                    return "user wins";
+                } else if (computerChoice === "paper") {
+                    return "computer wins";
+                } else {
+                    return "it's a tie";
+                }
+                
+        }
+        if (userChoice === "paper") {
+                if (computerChoice === "rock") {
+                    return "user wins";
+                } else if (computerChoice === "scissors") {
+                    return "computer wins";
+                } else {
+                    return "it's a tie";
+                }
+        }
+}
+
+var winner = selectWinner("scissors", "rock");
+
+//alert(winner);
+
 
 // *************************** YOUR CODE BELOW *******************************
 //******************TEST EARLY AND OFTEN USING console.log() ******************
@@ -35,3 +92,4 @@
 
 // DOCUMENT READY FUNCTION
 
+});
